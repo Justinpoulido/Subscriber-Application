7f322d933787bb4cf32598fd5e8edabe1e4c9880
justinpoulido <Jpoulido@gmail.com>
2024-11-13 00:33:27 -0400

    Additional Map Functionality was added.

--- app/src/main/java/com/example/subscriberapplication/MainActivity.kt
+++ app/src/main/java/com/example/subscriberapplication/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.subscriberapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+    val pointsList = mutableListOf<CustomMapPoint>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+        mMap.setOnMapClickListener { latLng ->
+            addMarkerAtLocation(latLng)
+            Log.d("MAPCLICK", "Map Clicked $latLng")
+
+        }
+        Log.d("Map ready", "Map has Loaded")
+    }
+
+    private fun addMarkerAtLocation( latLng: LatLng) {
+        val newCustomPoint = CustomMapPoint(pointsList.size + 1, latLng)
+        pointsList.add(newCustomPoint)
+
+        mMap.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title("Marker ${newCustomPoint.id}")
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+
+    }
+
 }
